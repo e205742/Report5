d5a1b15eac2e691b388b671ac753a0ae6c20e9b3
e205742 <e205742@ie.u-ryukyu.ac.jp>
2020-12-17 18:00:42 +0900

    Step2

--- src/jp/ac/uryukyu/ie/e205742/Main.java
+++ src/jp/ac/uryukyu/ie/e205742/Main.java
@@ -2,8 +2,13 @@ package jp.ac.uryukyu.ie.e205742;
 
 public class Main {
     public static void main(String[] args){
-        String str = null;
+        try{String str = null;
         str.length();
+        }
+        catch(NullPointerException n){
+            System.out.println("NullPointerExceptionが発生した");
+            System.out.println(n.getMessage());
+        }
 
     }
 }
